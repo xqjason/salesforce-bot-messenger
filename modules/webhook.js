8908a4e9f00eb825e06eae56e65bcaa9220a1a34
xqjason <jason.xuqing@hotmail.com>
2016-10-28 23:18:51 +0800

    update

--- modules/webhook.js
+++ modules/webhook.js
@@ -3,7 +3,7 @@
 let request = require('request'),
     salesforce = require('./salesforce'),
     formatter = require('./formatter-messenger');
-var fs = require("fs");
+
 
 let sendMessage = (message, recipient) => {
     request({
@@ -106,47 +106,8 @@ let handlePost = (req, res) => {
             }
             if (payload[0] === "attach_file")
             {
-                let filename = "image.png";
-
-                let base64data = "";
-
-                /*new Buffer(filename).toString('base64');*/
-
-                fs.readFile(filename, 'base64' , function (err, data) {
-                    if (err) throw err;
-                    base64data = data.toString()
-                });
-
-                
-
-                var root = "https://jsxin-dev-ed.my.salesforce.com/";
-                var url = root+"services/data/v37.0/sobjects/Attachment";
-
-                var data = {
-                  "Name" : filename,
-                  "Body": base64data,
-                  "parentId": payload[1] 
-                };
-
-                var xhttp = new XMLHttpRequest();
-
-                xhttp.open("POST", url, true);
-                xhttp.setRequestHeader('Content-Type', 'application/json');
-                xhttp.setRequestHeader('Authorization', 'Bearer ' + salesforce.org.oauth.access_token);
-                xhttp.send(data);
-
-                /*
-                req.setEndpoint(url);
-                req.setMethod("POST");
-                req.setHeader('Content-Type', 'application/json');
-                req.setHeader('Authorization', 'Bearer ' + salesforce.org.oauth.access_token);
-                var data = {
-                  "Name" : filename,
-                  "Body": base64data,
-                  "parentId": payload[1] 
-                }
-                req.setbody(data);
-                */
+                console.log("attach a file");
+                res.send("/attach");
             } 
         }
     }
